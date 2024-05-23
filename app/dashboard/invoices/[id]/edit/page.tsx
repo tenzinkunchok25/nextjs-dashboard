56c6486311d6d47f8e30c33dac6cd1d493694f9d
Tenzin Kunchok <tenkunchok012511@gmail.com>
2024-05-23 12:05:40 -0400

    Adding Metadata

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -3,6 +3,11 @@ import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
 import { updateInvoice } from '@/app/lib/actions';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
  
 export default async function Page({ params }: { params: { id: string } }) {
     const id = params.id;
